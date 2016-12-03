523e3d392e4ddca1daef35207c4bca75df20d62e
Lakmal Buddika Meegahapola <lakmalbuddikalucky@gmail.com>
2016-12-03 20:48:06 +0100

    Update 4
    Contains some major changes to the page structure and some additional
    images as well

--- app/app.js
+++ app/app.js
@@ -1,15 +1,26 @@
-// (function() {
-//     angular.module('MoodMove')
-//         .controller('AppController', ['$scope', '$http', '$state', function($scope, $http, $state) {
-//             $scope.sampleData = "";
-//             $scope.logUserIn = function() {
-//                 $http.post('api/user/login', $scope.sampleData)
-//                     .then(function(response) {
-//                         console.log(response);
-//                     })
-//                     .catch(function(fallback) {
-//                         //console.log(fallback);
-//                     })
-//             }
-//         }])
-// }());
+'use strict'
+
+var MoodMove = angular.module('MoodMove', ['ngRoute', 'ngResource']);
+
+MoodMove.controller('AppController', ['$scope', function($scope) {
+    $scope.selectedHour;
+    $scope.textHour = "hours"
+    $scope.changeHour = function(selectedHour) {
+        if (selectedHour == "1") {
+            $scope.textHour = "hour";
+        } else {
+            $scope.textHour = "hours";
+        }
+    }
+    $scope.hourList = {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9"
+    };
+}]);
